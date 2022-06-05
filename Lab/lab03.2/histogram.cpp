52137ebe705ccabc5f51ef9147d85a5f76a8596a
SemenchenkoND <aboba@mpei.ru>
2022-06-05 19:24:32 +0300

    code: вывод подсказок в зависимости от prompt

--- Lab/lab03.2/histogram.cpp
+++ Lab/lab03.2/histogram.cpp
@@ -1,7 +1,6 @@
 #include "histogram.h"
-#include <iostream>
-#include <vector>
-#include <string>
+
+
 using namespace std;
 
 void find_minmax(const vector<double>& numbers, double& min, double& max)
@@ -27,12 +26,121 @@ void find_minmax(const vector<double>& numbers, double& min, double& max)
 
 }
 
-vector<double> input_numbers(size_t number_count, istream& in)
+vector<size_t> make_histogram(Input data)
 {
-	vector<double> result(number_count);
-	for (size_t i = 0; i < number_count; i++)
+
+
+	double min, max;
+	find_minmax(numbers, min, max);
+
+
+	double bin_size = (max - min) / bin_count;
+	vector<size_t> bins(bin_count, 0);
+	for (size_t i = 0; i < numbers.size(); i++)
+	{
+		bool found = false;
+		for (size_t j = 0; j < (bin_count - 1) && !found; j++)
+		{
+			auto lo = min + j * bin_size;
+			auto hi = min + (j + 1) * bin_size;
+			if ((lo <= numbers[i]) && (numbers[i] < hi))
+			{
+				bins[j]++;
+				found = true;
+			}
+		}
+		if (!found)
+		{
+			bins[bin_count - 1]++;
+		}
+	}
+	return bins;
+}
+
+vector<double> input_numbers(istream& in, size_t count)
+{
+	vector<double> result(count);
+	for (size_t i = 0; i < count; i++)
 	{
 		in >> result[i];
 	}
 	return result;
+}
+
+Input read_input(istream& in, bool prompt)
+{
+	Input data;
+
+	size_t number_count;
+	if (prompt)
+	{
+		cerr << "Enter number count: ";
+	}
+	in >> number_count;
+
+	if (prompt)
+	{
+		cerr << "Enter numbers: ";
+	}
+	data.numbers = input_numbers(in, number_count);
+
+	if (prompt)
+	{
+		cerr << "Enter bin count: ";
+	}
+	in >> data.bin_count;
+
+
+
+	return data;
+}
+
+void show_histogram_text(vector <size_t> bins)
+{
+	const size_t SCREEN_WIDTH = 80;
+	const size_t MAX_ASTERISK = SCREEN_WIDTH - 3 - 1;
+
+	size_t max_bin = bins[0];
+	for (double bin : bins)
+	{
+		if (bin > max_bin)
+		{
+			max_bin = bin;
+		}
+	}
+
+	size_t maxhigth;
+	if (max_bin < 76)
+		maxhigth = max_bin;
+	else
+		maxhigth = 76;
+	for (double bin : bins)
+	{
+		size_t height = bin;
+		// проверить масштабирование(пересчитать height)
+
+		if (height > MAX_ASTERISK)
+		{
+			height = MAX_ASTERISK * (static_cast<double> (bin) / max_bin);
+		}
+
+
+		if (bin < 100) {
+			cout << " ";
+		}
+
+		if (bin < 10) {
+			cout << " ";
+		}
+		for (size_t i = 0; i < maxhigth - height; i++)
+			cout << " ";
+
+		for (size_t i = 0; i < height; i++)
+		{
+			cout << "*";
+		}
+		cout << "|" << bin << endl;
+
+	}
+
 }
